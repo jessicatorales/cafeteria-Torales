b33897bcabdd39f694b8e86616ddcc983810e727
jessicatorales <je-si@hotmail.es>
2022-12-13 11:41:48 -0300

    agregada logica del carrito y cambios en el estilo del sitio

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -1,50 +1,54 @@
 let lblCarrito = document.getElementById("lbl__carrito");
 let div__carrito = document.getElementById("div__carrito");
 
-let basket = JSON.parse(localStorage.getItem("data")) || [];
+let productos = JSON.parse(localStorage.getItem("productos"));
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-let calculation = () => {
+let calcularCarrito = () => {
   let carritoIcon = document.getElementById("carritoAmount");
-  carritoIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+  carritoIcon.innerHTML = carrito.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
 
-calculation();
+calcularCarrito();
 
-let generatecarritoItems = () => {
-  if (basket.length !== 0) {
-    return (div__carrito.innerHTML = basket
+let generarItemsCarrito = () => {
+  if (carrito.length !== 0) {
+    return (div__carrito.innerHTML = carrito
       .map((x) => {
         let { id, item } = x;
-        let search = shopItemsData.find((y) => y.id === id) || [];
+        let search = productos.find((y) => y.id === id) || [];
         return `
-      <div class="carrito-item">
-        <img width="100" src=${search.img} alt="" />
-        <div class="details">
+        <div class="carrito__item">
+          <img width="200px" src=${search.imagen} alt="" />
+          <div class="details">
+            <div class="title-precio-x">
+                <h4 class="title-precio">
+                  <p>${search.nombre}</p>
+                  <p class="carrito__item__precio">$ ${search.precio}</p>
+                </h4>
+                <i onclick="removeItem(${id})" class="bi bi-x-lg" style="
+                    background-color: #212529;
+                    color: red;
+                    margin: 10px;">
+                </i>
+            </div>
 
-          <div class="title-precio-x">
-              <h4 class="title-precio">
-                <p>${search.name}</p>
-                <p class="carrito-item-precio">$ ${search.precio}</p>
-              </h4>
-              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+            <div class="buttons">
+                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                <div id=${id} class="quantity">${item}</div>
+                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
+
+            <h3>$ ${item * search.precio}</h3>
           </div>
-
-          <div class="buttons">
-              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-              <div id=${id} class="quantity">${item}</div>
-              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-          </div>
-
-          <h3>$ ${item * search.precio}</h3>
         </div>
-      </div>
       `;
       })
       .join(""));
   } else {
     div__carrito.innerHTML = ``;
     lblCarrito.innerHTML = `
-    <h2>Tu carrito está vacio, ve a productos para agregar</h2>
+    <h2>Tu carrito esta vacio, ve a productos para agregar</h2>
     <a href="../pages/productos.html">
       <button class="btn__producto">Volver a Productos</button>
     </a>
@@ -52,78 +56,108 @@ let generatecarritoItems = () => {
   }
 };
 
-generatecarritoItems();
+generarItemsCarrito();
 
 let increment = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
+  let selectedItemId = id;
+  let search = carrito.find((x) => x.id === selectedItemId);
 
   if (search === undefined) {
-    basket.push({
-      id: selectedItem.id,
+    carrito.push({
+      id: selectedItemId,
       item: 1,
     });
   } else {
     search.item += 1;
   }
+  Toastify({
+    text: 'Has agregado un producto al Carrito'
+  }).showToast()
 
-  generatecarritoItems();
-  update(selectedItem.id);
-  localStorage.setItem("data", JSON.stringify(basket));
+  generarItemsCarrito();
+  actualizarItems(selectedItemId);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 };
+
 let decrement = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
+  let selectedItemId = id;
+  let search = carrito.find((x) => x.id === selectedItemId);
 
   if (search === undefined) return;
   else if (search.item === 0) return;
   else {
     search.item -= 1;
   }
-  update(selectedItem.id);
-  basket = basket.filter((x) => x.item !== 0);
-  generatecarritoItems();
-  localStorage.setItem("data", JSON.stringify(basket));
+  Toastify({
+    text: 'Has restado un producto al Carrito'
+  }).showToast()
+  actualizarItems(selectedItemId);
+  carrito = carrito.filter((x) => x.item !== 0);
+  generarItemsCarrito();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 };
 
-let update = (id) => {
-  let search = basket.find((x) => x.id === id);
+let actualizarItems = (id) => {
+  let search = carrito.find((x) => x.id === id);
   document.getElementById(id).innerHTML = search.item;
-  calculation();
-  MontoTotal();
+  calcularCarrito();
+  calcularMontoTotal();
 };
 
 let removeItem = (id) => {
-  let selectedItem = id;
-  basket = basket.filter((x) => x.id !== selectedItem.id);
-  generatecarritoItems();
-  MontoTotal();
-  localStorage.setItem("data", JSON.stringify(basket));
+  let selectedItemId = id;
+  carrito = carrito.filter((x) => x.id !== selectedItemId);
+  Toastify({
+    text: 'Has eliminado por completo un producto del Carrito'
+  }).showToast()
+  generarItemsCarrito();
+  calcularMontoTotal();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  calcularCarrito();
 };
 
-let limpiarcarrito = () => {
-  basket = [];
-  generatecarritoItems();
-  localStorage.setItem("data", JSON.stringify(basket));
+let limpiarCarrito = () => {
+  carrito = [];
+  generarItemsCarrito();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 };
 
-let MontoTotal = () => {
-  if (basket.length !== 0) {
-    let amount = basket
+let realizarCompra = () => {
+  let amount = carrito
+    .map((x) => {
+      let { item, id } = x;
+      let search = productos.find((y) => y.id === id) || [];
+
+      return item * search.precio;
+    })
+    .reduce((x, y) => x + y, 0);
+  div__carrito.innerHTML = ``;
+  lblCarrito.innerHTML = `
+      <h2>Tu compra se ha enviado exitosamente!</h2>
+      <h2>El monto total es de $${amount}</h2>
+      <h4>Para volver a nuestros productos, haz clic en el boton de abajo</h3>
+      <a href="../pages/productos.html">
+        <button class="btn__producto" onclick="limpiarCarrito()">Volver</button>
+      </a>
+    `;
+}
+
+let calcularMontoTotal = () => {
+  if (carrito.length !== 0) {
+    let amount = carrito
       .map((x) => {
         let { item, id } = x;
-        let search = shopItemsData.find((y) => y.id === id) || [];
+        let search = productos.find((y) => y.id === id) || [];
 
         return item * search.precio;
       })
       .reduce((x, y) => x + y, 0);
-    // console.log(amount);
     lblCarrito.innerHTML = `
     <h2>Monto Total : $ ${amount}</h2>
-    <button class="checkout">Comprar</button>
-    <button onclick="limpiarcarrito()" class="removeAll">Limpiar Carrito</button>
+    <button class="btn__Checkout" onclick="realizarCompra()">Comprar</button>
+    <button  class="btn__RemoveAll" onclick="limpiarCarrito()">Limpiar Carrito</button>
     `;
   } else return;
 };
 
-MontoTotal();
+calcularMontoTotal();
--- scripts/index.js
+++ scripts/index.js
@@ -1,131 +1,92 @@
 
 class Producto {
-    constructor(id, nombre, descripcion, precio, imagen, stock) {
+    constructor(id, nombre, descripcion, precio, imagen) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.imagen = imagen;
-        this.stock = stock;
-    }
-
-    restaStock() {
-        this.stock = this.stock - 1;
-    }
-}
-
-class Carrito {
-    constructor() {
-        this.id = id,
-            this.nombre = nombre,
-            this.cantidad = cantidad
     }
 }
 
 let productos = setearProductos();
-localStorage.setItem("productos", JSON.stringify(productos))
+localStorage.setItem("productos", JSON.stringify(productos));
 
-let infoProductos = JSON.parse(localStorage.getItem("productos"))
-let divProducto = document.getElementById("div__categoria")
+let infoProductos = JSON.parse(localStorage.getItem("productos"));
+let divProducto = document.getElementById("div__categoria");
 
-infoProductos.forEach(p => {
-    divProducto.innerHTML +=
-        `
-    <div class="div__producto">
-        <img class="img__producto" src="${p.imagen}">
-        <p class="p__producto__texto">${p.nombre}</p>
-        <p class="p__producto__precio">$ ${p.precio}</p>
-        <button id=${p.id} class="btn__producto">AGREGAR AL CARRITO</button>
-    </div>
-    `
-});
+let precioTotal = 0;
+let productosEnCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-let precioTotal = 0
-const carrito = []
-let productosEnCarrito = []
-
-let divBody = document.getElementById("div__body")
-let divCard = document.getElementById("div__card")
-let btnFinalizar = document.getElementById("btn__comprar")
-localStorage.setItem("productosEnCarrito", JSON.stringify(productosEnCarrito))
-
-function agregarProducto() {
-    divProducto.onclick = (e) => {
-        let _id = parseInt(e.target.id)
-        let productoElegido = productos.find(p => p.id === _id)
-        let temp = carrito.includes(productoElegido)
-        Toastify({
-            text: `El producto ${productoElegido.nombre} fue agregado al carrito`,
-            duration: 3000,
-            style: {
-                background: "#000"
-            }
-        }).showToast();
-
-        if (temp) {
-            productosEnCarrito = JSON.parse(localStorage.getItem("productosEnCarrito"))
-            let temp = productosEnCarrito.find(p => p.id === _id)
-            temp.cantidad = temp.cantidad + 1
-            localStorage.setItem("productosEnCarrito", JSON.stringify(productosEnCarrito))
-            return productosEnCarrito
-        } else {
-            carrito.push(productoElegido)
-            productosEnCarrito = JSON.parse(localStorage.getItem("productosEnCarrito"))
-            let productoAMostrar = { "id": _id, "nombre": productoElegido.nombre, "cantidad": 1 }
-            productosEnCarrito.push(productoAMostrar)
-            localStorage.setItem("productosEnCarrito", JSON.stringify(productosEnCarrito))
-        }
-        precioTotal += productoElegido.precio
-        localStorage.setItem("total", precioTotal)
-    }
-}
-
-function terminarCompra() {
-    btnFinalizar.onclick = () => {
-        precioTotal = localStorage.getItem("total")
-        divBody.innerHTML = `
-            <div id="div__listado">
-                <h1 id="titulo">CARRITO</h1>
+let generarProductos = () => {
+    return (divProducto.innerHTML = infoProductos
+        .map((x) => {
+            let { id, nombre, precio, descripcion, imagen } = x;
+            let search = productosEnCarrito.find((x) => x.id === id) || [];
+            return `
+            <div id=product-id-${id} class="div__producto">
+                <img class="img__producto" src=${imagen} alt="">
+                <div class="details">
+                    <p class="p__producto__texto">${nombre}</p>  
+                    <p class="p__producto__precio">$ ${precio}</p>
+                        <div class="precio-quantity">            
+                            <div class="buttons">
+                                <i onclick="restarStock(${id})" class="bi bi-dash-square-fill"></i>
+                                <div id=${id} class="quantity">
+                                    ${search.item === undefined ? 0 : search.item}
+                                </div>
+                                <i onclick="sumarStock(${id})" class="bi bi-plus-square-fill"></i>
+                            </div>
+                        </div>
+                </div>
             </div>
-            <div id="div__total"></div>
-            `
-        let divListado = document.getElementById("div__listado")
-        let divTotal = document.getElementById("div__total")
-        productosEnCarrito.forEach(p => {
-            console.log(p)
-            divListado.innerHTML += `<p class="h1">- ${p.nombre} (x${p.cantidad}) </p>`
+        `;
         })
-        divTotal.innerHTML += `
-        <p class= "h2">Total: $${precioTotal}</p> 
-        <button id="btnFinalizar">Comprar</button>`
+        .join(""));
+};
 
-        let btnFinalizar = document.getElementById("btnFinalizar")
-
-
-        btnFinalizar.onclick = () => {
-            location.reload()
-        }
-
-        localStorage.clear()
-        b = 0
-        precioTotal = 0
-        carrito = []
-    }
+let generarListaProductos = () => {
+    let { id, name, desc, precio, img } = x;
+    let search = productos.find((x) => x.id === id) || [];
+    infoProductos.forEach(p => {
+        divProducto.innerHTML +=
+            `
+        <div class="div__producto">
+            <img class="img__producto" src="${p.imagen}">
+            <p class="p__producto__texto">${p.nombre}</p>
+            <p class="p__producto__precio">$ ${p.precio}</p>
+            <div class="buttons">
+                  <i onclick="restarStock(${p.id})" class="bi bi-dash-square-fill"></i>
+                  <div id=${p.id} class="quantity">
+                  ${search.item === undefined ? 0 : search.item}
+                  </div>
+                  <i onclick="sumarStock(${p.id})" class="bi bi-plus-square-fill"></i>
+                </div>
+        </div>
+        `
+    });
 }
+
+let btnFinalizar = document.getElementById("btn__comprar")
+localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
+
+btnFinalizar.onclick = function () {
+    location.href = "../pages/carrito.html";
+};
 
 function setearProductos() {
-    const producto0 = new Producto(0, "Cafe en grano Bonafide 1kg", "Paquete color negro, cafe tostado", 4425, "../assets/cafeengrano1.jpg", 10);
-    const producto1 = new Producto(1, "Cafe molido Martinez 250g", "Tipo Italiano,fuerte", 1485, "../assets/cafeengrano3.jpg", 15);
-    const producto2 = new Producto(2, "Cafe tostado molido Cabrales 260g", "Libre de gluten, sin T.A.C.C.", 1690, "../assets/cafemolido1.jpg", 20);
+    const producto0 = new Producto(0, "Cafe en grano Bonafide 1kg", "Paquete color negro, cafe tostado", 4425, "../assets/cafeengrano1.jpg");
+    const producto1 = new Producto(1, "Cafe molido Martinez 250g", "Tipo Italiano,fuerte", 1485, "../assets/cafeengrano3.jpg");
+    const producto2 = new Producto(2, "Cafe tostado molido Cabrales 260g", "Libre de gluten, sin T.A.C.C.", 1690, "../assets/cafemolido1.jpg");
     const producto3 = new Producto(3, "Combo Cafe en capsulas Martinez 60u",
-        "Sabores: Vainilla,Avellana,Tipo Italiano,Moka,Brasil,Colombia", 8355, "../assets/capsulasdecafe1.jpg", 10);
+        "Sabores: Vainilla,Avellana,Tipo Italiano,Moka,Brasil,Colombia", 8355, "../assets/capsulasdecafe1.jpg");
     const producto4 = new Producto(4, "Cafe en capsulas Starbucks 20u",
-        "Sabores: Espresso Roast, House Blend, Pike Place Roast, Blonde", 4499, "../assets/capsulasdecafe2.jpg", 20);
-    const producto5 = new Producto(5, "Cafetera Moka Pedrini", "Negro Infinity para 6 posillos", 23053, "../assets/cafetera1.jpg", 5)
-    const producto6 = new Producto(6, "Moka Pedrini Alumini", "Rojo para 3 posillos", 12753, "../assets/cafetera2.jpg", 8 );
-    const producto7 = new Producto(7, "Jarra medidora 350ml", "Negra Acero Inoxidable", 6540, "../assets/jarramedidora1.jpg", 10);
-    const producto8 = new Producto(8, "Porta Cápsulas", "x24", 3902, "../assets/portacapsulas.jpg", 5);
-    const producto9 = new Producto(9, "Jarra Térmica Sakura", "Style Negra", 10692, "../assets/jarratermica3.jpg", 20);
+        "Sabores: Espresso Roast, House Blend, Pike Place Roast, Blonde", 4499, "../assets/capsulasdecafe2.jpg");
+    const producto5 = new Producto(5, "Cafetera Moka Pedrini", "Negro Infinity para 6 posillos", 23053, "../assets/cafetera1.jpg")
+    const producto6 = new Producto(6, "Moka Pedrini Alumini", "Rojo para 3 posillos", 12753, "../assets/cafetera2.jpg");
+    const producto7 = new Producto(7, "Jarra medidora 350ml", "Negra Acero Inoxidable", 6540, "../assets/jarramedidora1.jpg");
+    const producto8 = new Producto(8, "Porta Cápsulas", "x24", 3902, "../assets/portacapsulas.jpg");
+    const producto9 = new Producto(9, "Jarra Térmica Sakura", "Style Negra", 10692, "../assets/jarratermica3.jpg");
 
     let productos = []
     productos.push(producto0);
@@ -141,5 +102,55 @@ function setearProductos() {
 
     return productos;
 }
-agregarProducto();
+let sumarStock = (id) => {
+    let selectedItem = id;
+    let search = productosEnCarrito.find((x) => x.id === selectedItem);
+
+    if (search === undefined) {
+        productosEnCarrito.push({
+            id: selectedItem,
+            item: 1,
+        });
+    } else {
+        search.item += 1;
+    }
+
+    Toastify({
+        text: 'Has agregado un producto al carrito'
+    }).showToast()
+
+    actualizarProducto(selectedItem);
+    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito));
+};
+
+let restarStock = (id) => {
+    let selectedItem = id;
+    let search = productosEnCarrito.find((x) => x.id === selectedItem);
+
+    if (search === undefined) return;
+    else if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    Toastify({
+        text: 'Has restado un producto del carrito'
+    }).showToast()
+    actualizarProducto(selectedItem);
+    productosEnCarrito = productosEnCarrito.filter((x) => x.item !== 0);
+    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito));
+};
+
+let actualizarProducto = (id) => {
+    let search = productosEnCarrito.find((x) => x.id === id);
+    document.getElementById(id).innerHTML = search.item;
+    calcularCantidadCarrito();
+};
+
+let calcularCantidadCarrito = () => {
+    let carritoIcon = document.getElementById("carritoAmount");
+    carritoIcon.innerHTML = productosEnCarrito.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+generarProductos();
+calcularCantidadCarrito();
 terminarCompra();
